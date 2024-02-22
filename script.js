f8bf650449a96b7ba9b7ce103bd5f5a2469b754f
Sourabh Mistry <157691255+sourabhm7qloron@users.noreply.github.com>
2024-02-22 16:58:55 +0530

    script.js

--- script.js
+++ script.js
@@ -8,19 +8,28 @@ function checkLogin() {
     user_records.some((v) => {
       return v.email === email && v.password === password;
     })
-  ) 
-  
-  {
+  ) {
     alert("Login Successful");
-    window.location.href='dashboard.html';
-  } 
-  else if (email.indexOf("@") === -1) {
+    window.location.href = "dashboard.html";
+  } else if (!email.includes("@") || !email.includes(".com")) {
+    document.getElementById("email_error").textContent =
+      "Email should contain both '@' and '.com'";
     document.getElementById("email_error").style.display = "block";
-
-
-
-
+    document.getElementById("password_error").style.display = "none";
+  } else if (
+    !/[a-z]/.test(password) ||
+    !/[A-Z]/.test(password) ||
+    !/[0-9]/.test(password) ||
+    !/[^a-zA-Z0-9]/.test(password)
+  ) {
+    document.getElementById("email_error").style.display = "none";
+    document.getElementById("password_error").textContent =
+      "Password should contain atleast 1 uppercase,lowercase,special character, number";
+    document.getElementById("password_error").style.display = "block";
   } else {
+    document.getElementById("email_error").style.display = "none";
+    document.getElementById("password_error").textContent =
+      "Data not available. Please register first";
     document.getElementById("password_error").style.display = "block";
   }
 }
